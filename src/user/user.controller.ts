673c87897f2f00b612cc2a35e77ca834ae3ecae9
A2kmoise <abayomoise950@gmail.com>
2025-06-02 13:42:45 +0200

    usercontrol added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService:UserService){}
+
+    @Get('/:id')
+    getUserById(@Param('id') id:string){
+return this.userService.getUserById()
+    }
+
+    @Patch('/:id')
+    editUserById(@Param('id') id: string){
+return this.userService.editUserById()
+    }
+
+    @Delete('/:id')
+    deleteUserById(@Param('id') id:string){
+return this.userService.deleteUserById()
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+getUserById(){
+return 'Here is User'
+}
+
+deleteUserById(){
+ return 'deleted successfully'
+}
+editUserById(){
+    return 'edited successfully'
+}
+    
+}

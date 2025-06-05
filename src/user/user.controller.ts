05a56b7b67f0bd9569430650b036ab7bf772d6f2
A2kmoise <abayomoise950@gmail.com>
2025-06-05 20:54:00 +0200

    -[ * * ]- /me

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Delete, Get, Param, Patch, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDto } from './dto';
+import { User } from './user.entity';
+import { GetUser } from 'src/auth/decorator';
 
 @Controller('users')
 export class UserController {
     constructor(private userService:UserService){}
+
+    @Get('me')
+    getMe(@GetUser() user:User){
+     return user;
+    }
 
     @Get(':id')
     getUserById(@Param('id') id:number){

2506e9522379c6ee94a5fb3f3127705f84acb727
A2kmoise <abayomoise950@gmail.com>
2025-06-06 22:01:06 +0200

    -[ * * ]- task CRUD

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
+import { Task } from './task.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Task, User])],
   controllers: [TaskController],
   providers: [TaskService]
 })
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,26 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './task.entity';
+import { Repository } from 'typeorm';
+import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
+import { title } from 'process';
 
 @Injectable()
 export class TaskService {
+    constructor(@InjectRepository(Task) private taskRepository: Repository<Task>) { }
 
+    async createTask(dto) {
+        //find if task exists
+        const extinguishtask = await this.taskRepository.findOne({ where: { title: dto.title } });
+        if (extinguishtask) throw new ConflictException('Task already exists');
+        //create a task
+        const task = await this.taskRepository.create({
+            title: dto.title,
+            description: dto.description
+        });
 
-    createTask(dto){
-        return 'Task created';
+        await this.taskRepository.save(task);
+
+        return {
+            message: "Task created successfully",
+            task: {
+                title: task.title,
+                description: task.description
+            }
+        }
+
     }
 
-    getTask(){
-        return 'All tasks here';
+    async getTask() {
+      //retrieve all tasks
+    const tasks = await this.taskRepository.find();
+
+    return{
+        message: "All tasks",
+        tasks
+    }
     }
 
-    getTaskById(id){
-        return 'One task retrived';
+    async getTaskById(id) {
+        //find if task exists
+        const task = await this.taskRepository.findOne({ where: id});
+        if (!task) throw new NotFoundException('This task doesn\'t exist');
+
+        return {
+            message: "The single task retrieved",
+            task: {
+                id: task.id,
+                title: task.title,
+                description: task.description
+            }
+        }
     }
 
-    updateTaskById(id){
-        return 'Task updated';
+   async  updateTaskById(id) {
+        // find if task exists
+        const task =  await this.taskRepository.findOne({where:{ id }});
+        if (!task) throw new NotFoundException('This task doesn\'t exist');
+           //update a task
+
+      /*  const updatedtask = await this.taskRepository.update({
+
+        })*/
     }
 
-    deleteTaskById(id){
+    deleteTaskById(id) {
         return 'Task deleted';
     }
 }
